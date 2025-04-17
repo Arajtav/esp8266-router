36df50234a63cd422d01901a5ed2951d4639dc29
Aräjtav <arajtav@arajtav.com>
2025-04-17 22:22:36 +0200

    feat: log device connecting and disconnecting, get ssid and pass from config file

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,15 @@
 
 static File log_file;
 
+WiFiEventHandler stationConnectedHandler;
+WiFiEventHandler stationDisconnectedHandler;
+
+String macToString(const unsigned char* mac) {
+    char buf[18]; // 6*2 (numbers) + 5 (colons) + 1 (null terminator)
+    snprintf(buf, 18, "%02x:%02x:%02x:%02x:%02x:%02x", MAC2STR(mac));
+    return String(buf);
+}
+
 void clog(const char* level, const char* message) {
     String entry = String(millis()) + " [" + level + "] " + message + "\n";
     if (log_file) log_file.write(entry.c_str());
@@ -24,9 +33,48 @@ void clog(const char* level, const String& message) {
 
 void cpanic(const char* message) {
     clog(LOG_CRITICAL, message);
+    log_file.flush();
     delay(1000);
     ESP.deepSleep(0);
 }
+
+static String admin_username;
+static String admin_password;
+static String wifi_ssid;
+static String wifi_pass;
+
+void read_wifi() {
+    File file = LittleFS.open("/config/wifi", "r");
+    if (!file) cpanic("missing `/config/wifi`");
+    String line = file.readStringUntil('\n');
+    file.close();
+    int separatorIndex = line.indexOf(':');
+    if (separatorIndex < 0) cpanic("`config/wifi` is invalid");
+    wifi_ssid = line.substring(0, separatorIndex);
+    wifi_pass = line.substring(separatorIndex + 1);
+}
+
+void read_admin() {
+    File file = LittleFS.open("/config/admin", "r");
+    if (!file) cpanic("missing `/config/admin`");
+    String line = file.readStringUntil('\n');
+    file.close();
+    int separatorIndex = line.indexOf(':');
+    if (separatorIndex < 0) cpanic("`config/admin` is invalid");
+    admin_username = line.substring(0, separatorIndex);
+    admin_password = line.substring(separatorIndex + 1);
+}
+
+// TODO: LOG IP
+void onStationConnected(const WiFiEventSoftAPModeStationConnected& evt) {
+    clog(LOG_INFO, "Station connected: " + macToString(evt.mac));
+}
+
+void onStationDisconnected(const WiFiEventSoftAPModeStationDisconnected& evt) {
+    clog(LOG_INFO, "Station disconnected: " + macToString(evt.mac));
+}
+
+IPAddress ap_ip(192, 168, 0, 1);
 
 void setup() {
     Serial.begin(9600);
@@ -36,12 +84,19 @@ void setup() {
     log_file = LittleFS.open("/log.txt", "a+");
     if (!log_file) cpanic("Failed to open log file");
 
-    File admin_file = LittleFS.open("/config/admin", "r");
-    if (!admin_file) cpanic("missing `/config/admin`");
+    clog(LOG_INFO, "Started logfile");
+    read_admin();
+    read_wifi();
+    clog(LOG_INFO, "Starting AP, with SSID: `" + wifi_ssid + "` and password `" + wifi_pass + "`");
 
-    clog(LOG_INFO, "admin entry:");
-    while (admin_file.available()) clog(LOG_INFO, admin_file.readStringUntil('\n'));
-    admin_file.close();
+    WiFi.persistent(false);
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(ap_ip, ap_ip, IPAddress(255, 255, 255, 0));
+    WiFi.softAP(wifi_ssid, wifi_pass);
+    clog(LOG_INFO, "AP started, IP address: " + WiFi.softAPIP().toString() + " (should be " + ap_ip.toString() + ")"); // TODO: print ssid and password
+
+    stationConnectedHandler = WiFi.onSoftAPModeStationConnected(&onStationConnected);
+    stationDisconnectedHandler = WiFi.onSoftAPModeStationDisconnected(&onStationDisconnected);
 }
 
 void loop() {
